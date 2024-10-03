a22467c80418434706d1603d4985d675b3c0f0c8
seut <2841185054@qq.com>
2024-10-03 16:40:01 +0800

    24.10.3

--- src/components/person.vue
+++ src/components/person.vue
@@ -3,10 +3,12 @@
     <h2>姓名:{{ name }}</h2>
     <h2>年龄:{{ age }}</h2>
     <h2>电话:{{ tel }}</h2>
-
+    <button @click="changename">修改名字</button>
+    <button @click="changeage">修改年龄</button>
+    <button @click="changetel">修改电话</button>
 </div>
 </template>
-<script>
+<script lang="ts">
   export default {//此处语法检查严格要求有一个空白分隔符
   name:'person'
 } 
@@ -17,6 +19,16 @@ import { ref } from 'vue';
 let name = ref('张三')
 let age = 18
 let tel = '18475996255'
+function changename(){
+  name.value='zhangsan'
+}
+function changeage(){
+  age = 19
+}
+function changetel()
+{
+  tel='14578555422'
+}
 </script>
 <style scoped>
 .person{
